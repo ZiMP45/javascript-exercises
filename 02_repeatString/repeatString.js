26a4bc3ea16c72494ed94803ab851e1af97f6bf5
Zak Porche <93185579+ZiMP45@users.noreply.github.com>
2023-06-18 18:45:47 +0000

    practice

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+    let newString = '';
+    if (times < 0) {
+        return "ERROR";
+    }
+    for (let i = 1; i <= times; i++) {
+        newString += word;
+    }
 
+    return newString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let myArr = string.split("");
+    string = "";
+    for (let i = myArr.length - 1; i >= 0; i--) {
+        string += myArr[i];
+    }
 
+    return string;
 };
 
 // Do not edit below this line
