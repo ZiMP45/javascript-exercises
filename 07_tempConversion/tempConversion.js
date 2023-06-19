937066b2e5310e244248333197c1cf988ed448c1
Zak Porche <93185579+ZiMP45@users.noreply.github.com>
2023-06-19 17:32:06 +0000

    practice

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  let celsius = (fTemp - 32) * (5/9);
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  let fahrenheit = (cTemp * (5/9) + 32);
+  return fahrenheit;
 };
 
 // Do not edit below this line
