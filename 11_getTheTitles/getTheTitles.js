0fd55905df46961f2c5f05864f263ca2144bdeac
Zak Porche <93185579+ZiMP45@users.noreply.github.com>
2023-06-30 18:42:15 +0000

    found a different way but inefficient

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -8,5 +8,11 @@ const getTheTitles = function(books) {
     return bookTitles;
 };
 
+// alternatively per solution
+// return books.map((book) => book.title);
+// .map is a copying method, and using the arrow function within
+// it returns a new copied array without having to explicitly say that
+// it's doing so.
+
 // Do not edit below this line
 module.exports = getTheTitles;
