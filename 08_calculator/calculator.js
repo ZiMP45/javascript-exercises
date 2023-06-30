51cb0a143091e7cb614d541bd6a8dee5d915fb35
Zak Porche <93185579+ZiMP45@users.noreply.github.com>
2023-06-30 04:32:58 +0000

    calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...arr) {
+  let sum = 1;
+  for (let i = 0; i < arr.length; i++) {
+    sum *= arr[i];
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = num;
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+
+  while (num > 1) {
+    num--;
+    result *= num;
+  }
+  return result;
 };
 
 // Do not edit below this line
